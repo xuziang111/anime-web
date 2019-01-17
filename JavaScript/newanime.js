8186d0a79013f6332c6052b35df9c6e90b81fe7f
xuziang111 <584788096@qq.com>
2019-01-17 20:18:38 +0800

    删除冗余

--- JavaScript/newanime.js
+++ JavaScript/newanime.js
@@ -10,13 +10,6 @@ const newanime = Vue.component('newanime', {
 </ul>
 </nav>
 <section class="anime-con-con">
-    <div class="anime-container">
-        <div class="anime-img-con" ><a href="#"><img class="anime-img" src="../Images/201812311546250656.png"></a></div>
-        <div class="anime-title-con">
-            <h4>辉夜姬想让人告白 天才们的恋爱头脑战</h4>
-            <span>更新至1话</span>
-        </div>
-    </div>
     <div class="anime-container" v-for="item in animedata.data">
         <div class="anime-img-con" ><a :href="item.href"><img class="anime-img" :src=item.img></a></div>
         <div class="anime-title-con">
